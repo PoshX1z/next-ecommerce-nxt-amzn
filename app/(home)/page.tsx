755f3fe12d447cb1ef39b4fd2e595b4c73d891cc
PoshX1z <xdante1357@gmail.com>
2025-02-14 22:22:38 +0700

    Do carousal

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,10 @@
+import HomeCarousal from "@/components/shared/home/HomeCarousal";
+import data from "@/lib/data";
+
 export default async function Page() {
   return (
     <div>
-      <h1 className="h1-bold text-center p-10">Home Page Content</h1>
+      <HomeCarousal items={data.carousels} />
     </div>
   );
 }
