5897fc4e34561fb1e4763732a6c7350371d60d2a
PoshX1z <xdante1357@gmail.com>
2025-04-05 15:31:39 +0700

    Create sign-in-functions and pages

--- components/shared/header/Menu.tsx
+++ components/shared/header/Menu.tsx
@@ -1,17 +1,12 @@
-import { UserIcon } from "lucide-react";
-import Link from "next/link";
 import React from "react";
 import CartButton from "./CartButton";
+import UserButton from "./UserButton";
 
 const Menu = () => {
   return (
     <div className="flex justify-end">
       <nav className="flex gap-3 w-full">
-        <Link href="/signin" className="header-button">
-          <UserIcon className="h-8 w-8" />
-          <span className="font-bold">Sign in</span>
-        </Link>
-
+        <UserButton />
         <CartButton />
       </nav>
     </div>
