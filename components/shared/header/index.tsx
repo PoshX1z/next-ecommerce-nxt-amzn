b873ac1cd3ae982e689fe9dc29f8f1ef4e8456c8
PoshX1z <xdante1357@gmail.com>
2025-04-09 10:23:35 +0700

    Create category-sidebar

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -5,10 +5,11 @@ import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
 import Search from "./Search";
 import data from "@/lib/data";
-import { Button } from "@/components/ui/button";
-import { MenuIcon } from "lucide-react";
+import Sidebar from "./Sidebar";
+import { getAllCategories } from "@/lib/actions/product.actions";
 
-const Header = () => {
+const Header = async () => {
+  const categories = await getAllCategories();
   return (
     <header className="bg-black  text-white">
       <div className="px-2">
@@ -37,13 +38,7 @@ const Header = () => {
         </div>
       </div>
       <div className="flex items-center px-3 mb-[1px]  bg-gray-800">
-        <Button
-          variant="ghost"
-          className="header-button flex items-center gap-1 text-base [&_svg]:size-6"
-        >
-          <MenuIcon />
-          All
-        </Button>
+        <Sidebar categories={categories} />
         <div className="flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]">
           {data.headerMenus.map((menu) => (
             <Link
